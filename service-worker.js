7c4ccc079a223476b4a6cbbacb4f5c2cf9951879
ltrafalgar1 <ltrafalgar1@gmail.com>
2025-01-03 14:49:06 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,9 @@ const urlsToCache = [
     '/manifest.json',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
-    'https://cdn.jsdelivr.net/npm/@supabase/supabase-js',
 ];
 
-// Install Service Worker dan cache file statis
+// Install Service Worker and cache static files
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -18,7 +17,7 @@ self.addEventListener("install", event => {
     self.skipWaiting();
 });
 
-// Aktivasi Service Worker dan hapus cache lama
+// Activate Service Worker and remove old caches
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
@@ -34,41 +33,12 @@ self.addEventListener('activate', event => {
     self.clients.claim();
 });
 
-// Tangani permintaan fetch dengan caching dan refresh data
+// Handle fetch requests with caching and refresh data
 self.addEventListener('fetch', event => {
     const url = new URL(event.request.url);
 
-    // Caching untuk permintaan ke Supabase (dengan pengecekan kadaluarsa cache)
-    if (url.origin === 'https://lukqiyskvaeuqxtpkwax.supabase.co') {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(async cache => {
-                const cachedResponse = await cache.match(event.request);
-                const now = Date.now();
-
-                // Gunakan cache jika belum kadaluarsa (1 jam)
-                if (cachedResponse) {
-                    const cachedTime = new Date(cachedResponse.headers.get('sw-fetched-time'));
-                    if (now - cachedTime.getTime() < 1 * 60 * 60 * 1000) { // Cache 1 jam
-                        return cachedResponse;
-                    }
-                }
-
-                // Ambil data dari jaringan jika cache sudah kedaluwarsa
-                const fetchResponse = await fetch(event.request);
-                const responseClone = fetchResponse.clone(); // Salin respons sebelum dipakai
-                const headers = new Headers(fetchResponse.headers);
-                headers.append('sw-fetched-time', new Date().toISOString());
-                const response = new Response(responseClone.body, { headers });
-
-                // Simpan respons ke cache
-                cache.put(event.request, response.clone());
-
-                return fetchResponse;
-            })
-        );
-    }
-    // Default: caching untuk file statis dan CDN
-    else if (url.protocol === 'http:' || url.protocol === 'https:') {
+    // Default: caching for static files and CDN
+    if (url.protocol === 'http:' || url.protocol === 'https:') {
         event.respondWith(
             caches.match(event.request).then(response => {
                 return response || fetch(event.request).then(fetchResponse => {
