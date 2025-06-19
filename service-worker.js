1c48a9a6c96981c054d38e976c2f5f59dfe68e42
ltrafalgar1 <ltrafalgar1@gmail.com>
2025-06-19 08:53:59 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/manifest.json',
-    '/src//styles.css',
-    '/src//script.js',
+    '/src/styles.css',
+    '/src/script.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
 ];
